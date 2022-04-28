ee228d44a5407cb1b09d84eabb547fdb4d632135
Kaldybayev <96066331+Kaldybayev@users.noreply.github.com>
2022-04-28 21:41:25 +0600

    Add files via upload

---
+++ home_task1.dart
@@ -0,0 +1,124 @@
+import 'package:home_task1/delimeters_calculator.dart';
+import 'package:home_task1/numbers_transformer.dart';
+import 'package:home_task1/string_utilities.dart';
+import 'package:home_task1/point.dart';
+import 'package:home_task1/user.dart';
+
+void printHomework() {
+  var a = 124;
+  var b = -234;
+  var c = '110111010101';
+  var string = 'word 1 3 word 2 word 4321 21111 word';
+  var wordsList = ['a', 'b', 'a', 'a', 'one', 'one', 'one', 'three', 'six'];
+  var x = 3;
+  var y = 0;
+  var number = 27;
+
+  var divider = '------------------';
+
+  print('Домашняя работа №2.6');
+
+  print(divider);
+  print('Task №1');
+
+  var calculator = DelimetersCalculator();
+  print('НОД($a, $b) = ${calculator.gcd(a, b)}');
+  print('НОК($a, $b) = ${calculator.lcm(a, b)}');
+  print('Простые множители $a = ${calculator.factors(a)}');
+
+  print(divider);
+  print('Task №2');
+
+  var transformer = NumbersTransformer();
+  print('$a в двоичной системе = ${transformer.intToBinary(a)}');
+  print('$c в десятичной системе = ${transformer.binaryToInt(c)}');
+
+  print(divider);
+  print('Task №3');
+
+  var utils = StringUtilities();
+  print('Числа в строке "$string": ${utils.findNumsIn(string)}');
+
+  print(divider);
+  print('Task №4');
+
+  print('Вхождения слов в коллекцию: ${utils.findStringsIn(wordsList)}');
+
+  print(divider);
+  print('Task №5');
+
+  print('Цифры без повторений в коллекции: ${utils.findNumNamesIn(wordsList)}');
+
+  print(divider);
+  print('Task №6');
+
+  var point = Point(1, 2, 3);
+  print('Расстояние между точками: ${point.distanceTo(Point(2, 3, 3))}');
+
+  print(divider);
+  print('Task №7');
+
+  try {
+    print('Корень $x степени от числа $number: ${number.rootPower(x)}');
+    print('Корень $y степени от числа $number:');
+    print('${number.rootPower(y)}');
+  } catch (e) {
+    print(e);
+  }
+
+  print(divider);
+  print('Task №8');
+
+  var userManager = UserManager<User>();
+
+  final userA = User('a@a.ru');
+
+  final userList = [
+    User.empty(),
+    userA,
+    User('b@b.ru'),
+    AdminUser.empty(),
+    AdminUser('c@c.ru'),
+    AdminUser('d@d.ru'),
+    GeneralUser.empty(),
+    GeneralUser('e@e.ru')
+  ];
+
+  userManager.add(userList);
+
+  userManager.removeOne(userA);
+
+  print('Список адресов эл. почты: ${userManager.getEmails()}');
+}
+
+extension on num {
+  num mabs(num x) {
+    return x < 0 ? -x : x;
+  }
+
+  num rootPower(num rootDegree) {
+    if (rootDegree == 0) {
+      throw ArgumentError('Нельзя определить корень нулевой степени');
+    }
+
+    num eps = 0.1; // Допустимая погрешность
+    num root = this / rootDegree; // Начальное приближение корня
+    num rn = this; // Значение корня последовательным делением
+
+    while (mabs(root - rn) >= eps) {
+      rn = this;
+
+      for (int i = 1; i < rootDegree; i++) {
+        rn = rn / root;
+      }
+
+      root = 0.5 * (rn + root);
+    }
+
+    return root;
+  }
+}
+
+void main(List<String> arguments) {
+  printHomework();
+}
